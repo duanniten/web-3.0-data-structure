522c7607813df827c3561dab1e44159475299d97
Duann Iten <duann_@hotmail.com>
2025-04-27 11:46:34 -0300

    Make the test for the proof

--- test/merkleTree.test.js
+++ test/merkleTree.test.js
@@ -7,26 +7,26 @@ class MerkleTree {
         return this.makeTree(this.leaves)
     }
 
-    getProof(){
-        
+    getProof() {
+
     }
 
-    makeTree(leaves){
+    makeTree(leaves) {
 
-        if(leaves.length == 1){
+        if (leaves.length == 1) {
             return leaves[0];
         }
         let new_leaves = []
-        for(var i = 0; i < leaves.length; i += 2 ){
+        for (var i = 0; i < leaves.length; i += 2) {
             const left = leaves[i]
-            const right = leaves[i+1]
-            if (!right){
-                new_leaves.push(left)    
+            const right = leaves[i + 1]
+            if (!right) {
+                new_leaves.push(left)
             }
-            else{
+            else {
                 new_leaves.push(this.concat(left, right))
             }
-            
+
         }
         return this.makeTree(new_leaves)
     }
@@ -34,25 +34,38 @@ class MerkleTree {
 
 module.exports = MerkleTree;
 
-describe('merkle', function() {
+describe('merkle', function () {
     const concat = (a, b) => `Hash(${a} + ${b})`;
-    it('should create a root from two leaves: [A,B]', function() {
-      const leaves = ['A', 'B'];
-      const merkleTree = new MerkleTree(leaves, concat);
-  
-      expect(merkleTree.getRoot()).toBe("Hash(A + B)");
+    it('should create a root from two leaves: [A,B]', function () {
+        const leaves = ['A', 'B'];
+        const merkleTree = new MerkleTree(leaves, concat);
+
+        expect(merkleTree.getRoot()).toBe("Hash(A + B)");
     });
-    it('should handle a nase case [A]',()=>{
+    it('should handle a nase case [A]', () => {
         const leaves = ['A']
         const merkleTree = new MerkleTree(leaves, concat);
 
         expect(merkleTree.getRoot()).toBe("A");
     })
 
-    it('should create a root from two leaves: [A,B,C]', function() {
+    it('should create a root from two leaves: [A,B,C]', function () {
         const leaves = ['A', 'B', 'C'];
         const merkleTree = new MerkleTree(leaves, concat);
-    
+
         expect(merkleTree.getRoot()).toBe("Hash(Hash(A + B) + C)");
-      });
+    });
+
+    it('should return the proof from a index', () => {
+        const leaves = ['A', 'B', 'C', 'D', 'E'];
+        const merkleTree = new MerkleTree(leaves, concat);
+        const index = 2;
+        const expect_proof = [
+            { data: 'D', left: false },
+            { data: 'AB', left: true },
+            { data: 'E', left: false }
+        ]
+
+        expect(merkleTree.getProof(index)).toBe(expect_proof)
+    })
 });
